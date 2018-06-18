88ba0d069ecc35e3e913fb1d7d4a7fdcc89c5500
Ksenia <sg.o.hlukhaniuk@gmail.com>
2018-06-18 19:40:46 +0300

    plugin base created

---
+++ current-special.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Current Special Page for www.alliedbuildings.com
+Description: Add custom post type for the scheduled page.
+Version: 1.0
+Author: AHAPX
+License: GPL2
+*/
+
+require_once("cs_special.php");
+require_once("cs_options.php");
+require_once("cs_page_template.php");
+
+$special = new Special();
+$options = new Options();
+
+?>
